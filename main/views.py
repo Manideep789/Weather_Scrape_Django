f61e6d7a88fefafbe236215d2f895fcab9a708fa
MANIDEEP TONANGI <manideeptonangi@gmail.com>
2021-06-15 10:29:35 +0530

    initial commit

---
+++ main/views.py
@@ -0,0 +1,44 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+
+
+def get_html_content(city):
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+
+    city = city.replace(' ', '+')
+    html_content = session.get(
+        f'https://www.google.com/search?q=weather+{city}').text
+    # print(html_content)
+    return html_content
+
+
+def home(request):
+    result = None
+    if 'city' in request.GET:
+        # then fetch the data
+        city = request.GET.get('city')
+        html_content = get_html_content(city)
+        # print(html_content)
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        result = dict()
+        # extract region
+        result['region'] = soup.find(
+            "span", attrs={"class": "BNeawe tAd8D AP7Wnd"}).text
+        # extract temperature now
+        result['temp_now'] = soup.find(
+            "div", attrs={"class": "BNeawe iBp4i AP7Wnd"}).text
+        # get the day, hour and actual weather
+        result['dayhour'], result['weather_now'] = soup.find("div", attrs={"class": "BNeawe tAd8D AP7Wnd"}).text.split(
+            '\n')
+
+    return render(request, 'main/home.html', {'result': result})
